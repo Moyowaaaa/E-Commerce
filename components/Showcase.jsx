c5395d6d898fbcd872bca233f81007ed07204dec
Moyowaaaa <moyowa7@icloud.com>
2022-06-11 12:03:43 +0100

    mobile responsiveness

--- components/Showcase.jsx
+++ components/Showcase.jsx
@@ -11,11 +11,11 @@ const Showcase = ({ products, loader, openStore }) => {
   return (
     <div className="     w-full">
       {products ? (
-        <div className="flex flex-row justify-center flex-row my-6">
+        <div className="flex flex-col lg:flex-row justify-center flex-row my-6">
           {products &&
             products.map((product) => (
               <div
-                className="w-3/12 h-[25rem] hover:border-2 border-[blue] mx-2 h-full w-full flex flex-col lg:flex-row"
+                className="w-12/12 px-2 lg:px-0 mx-auto lg:w-3/12 h-[25rem] hover:border-2 border-[blue] lg:mx-2 h-full w-full flex flex-col lg:flex-row"
                 key={product.id}
               >
                 <div className="w-full flex-col flex items-center justify-center cursor-pointer">
@@ -27,7 +27,7 @@ const Showcase = ({ products, loader, openStore }) => {
                     <div className="flex flex-col h-3/4  py-[auto] items-center justify-center cursor-pointer">
                       <img src={product.image} height={200} width={150} />
 
-                      <div className="flex flex-col jusitfy-center w-full mx-[auto] items-center">
+                      <div className="flex flex-col jusitfy-center w-full mx-[auto] lg:items-center">
                         {product.title}
                         <br />
                         <p className="font-bold my-[auto]">{product.price}$</p>
